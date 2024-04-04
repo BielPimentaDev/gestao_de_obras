c4c4f37c5b834034a8cdd85091d36ab1dd4fa1a9
Gabriel Pimenta <gabrielpimenttas@gmail.com>
2024-04-04 17:00:40 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,195 @@
-import Image from "next/image";
+import Image from 'next/image';
+import {
+	ArrowDownIcon,
+	ArrowRightStartOnRectangleIcon,
+	Bars3Icon,
+	BriefcaseIcon,
+	BuildingOfficeIcon,
+	CheckBadgeIcon,
+	ChevronDownIcon,
+	ClipboardDocumentListIcon,
+	CurrencyDollarIcon,
+	DocumentTextIcon,
+	MagnifyingGlassIcon,
+	PencilIcon,
+	PencilSquareIcon,
+	PlusIcon,
+	ShoppingBagIcon,
+	ShoppingCartIcon,
+	TruckIcon,
+	UserCircleIcon,
+	UserIcon,
+} from '@heroicons/react/24/solid';
+import RoomCard from '@/components/card/RoomCard';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	return (
+		<main className='flex gap-12 bg-gray-100 mx-auto max-w-[1640px] items-start '>
+			<nav className='lg:block hidden fixed w-[280px] shadow-xl bg-white  pt-8 px-6 pb-16'>
+				<h1 className='font-bold text-3xl text-purple-800 text-center mb-8'>
+					Gestão de Obras
+				</h1>
+				<div className='flex flex-col gap-12'>
+					<div className='font-semibold'>
+						<h3 className='font-semibold text-gray-400 mb-3'>Gestao</h3>
+						<ul className='flex flex-col gap-2'>
+							<li className='flex items-center gap-1 text-purple-800 cursor-pointer'>
+								<BuildingOfficeIcon className='w-5 ' />
+								<p>Obras</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<DocumentTextIcon className='w-5 text-gray-400' />
+								<p>Relatorios</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<CurrencyDollarIcon className='w-5 text-gray-400' />
+								<p>Financeiro</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<ShoppingCartIcon className='w-5 text-gray-400' />
+								<p>Cotacao de materiais</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<BuildingOfficeIcon className='w-5 text-gray-400' />
+								<p>Conteudos</p>
+							</li>
+						</ul>
+					</div>
+					<div className='font-semibold'>
+						<h3 className='font-semibold text-gray-400 mb-3'>CADASTRO</h3>
+						<ul className='flex flex-col gap-2'>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<UserIcon className='w-5 ' />
+								<p>Mão de obra</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<DocumentTextIcon className='w-5 text-gray-400' />
+								<p>Equipamentos</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<TruckIcon className='w-5 text-gray-400' />
+								<p>Fornecedores</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<ClipboardDocumentListIcon className='w-5 text-gray-400' />
+								<p>Atas de reunião</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<PencilSquareIcon className='w-5 text-gray-400' />
+								<p>Contratos</p>
+							</li>
+						</ul>
+					</div>
+					<div className='font-semibold'>
+						<h3 className='font-semibold text-gray-400 mb-3'>
+							PERSONALIZAÇÕES
+						</h3>
+						<ul className='flex flex-col gap-2'>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<CheckBadgeIcon className='w-5 ' />
+								<p>Checklist de obras</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<PencilIcon className='w-5 text-gray-400' />
+								<p>Layout relatorio cliente</p>
+							</li>
+						</ul>
+					</div>
+					<div className='font-semibold'>
+						<h3 className='font-semibold text-gray-400 mb-3'>CONFIGURAÇÕES</h3>
+						<ul className='flex flex-col gap-2'>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<UserCircleIcon className='w-5 ' />
+								<p>Meu perfil</p>
+							</li>
+							<li className='flex items-center gap-3 text-gray-400 cursor-pointer'>
+								<BriefcaseIcon className='w-5 text-gray-400' />
+								<p>Minha empresa</p>
+							</li>
+						</ul>
+					</div>
+					<div className='flex items-center gap-1 text-red-500 font-bold'>
+						<ArrowRightStartOnRectangleIcon className='w-6' />
+						<p>Sair</p>
+					</div>
+				</div>
+			</nav>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+			<div className='lg:mr-12 mt-4 lg:pl-4 flex-1 lg:ml-[300px]'>
+				<header className='flex items-center lg:justify-between mb-8 lg:mr-0 mx-4 '>
+					<button className='lg:hidden block mr-6'>
+						<Bars3Icon className='w-6' />
+					</button>
+					<h2 className='font-bold text-3xl'>Obras</h2>
+					<h2 className='lg:ml-0 ml-auto'>
+						Olá, <span className='font-bold'>Henrique!</span>
+					</h2>
+				</header>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+				<div className='flex gap-4 items-end justify-between mb-12 lg:mx-0 mx-4'>
+					<div className='flex gap-2'>
+						<div className='relative p-1 border-gray-400 border rounded-lg appearance-none'>
+							<input
+								placeholder='Buscar obra...'
+								className='bg-inherit p-1 pl-8 pr-12 w-full text-sm font-medium appearance-none  '
+							/>
+							<MagnifyingGlassIcon className='w-5 text-purple-800 font-bold absolute left-2 top-1/2 -translate-y-1/2' />
+						</div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+						<button className='font-bold text-purple-800 text-sm'>
+							Buscar
+						</button>
+					</div>
+					<div className='lg:block hidden'>
+						<p className='font-bold text-sm mb-2'>Ordenaçao</p>
+						<div className='relative'>
+							<select
+								className='-red-500 appearance-none block p-2 pl-4 pr-12 bg-inherit mt-1 border border-gray-400 rounded-lg  '
+								name='select'
+								id='select'>
+								<option disabled selected hidden value='opcao1'>
+									A - Z
+								</option>
+								<option value='opcao2'>B</option>
+								<option value='opcao3'>C</option>
+							</select>
+							<ChevronDownIcon className='w-4 absolute right-4 top-1/2 -translate-y-1/2' />
+						</div>
+					</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+					<div className='lg:block hidden'>
+						<p className='font-bold text-sm mb-2'>Status da obra</p>
+						<div className='relative'>
+							<select
+								className='-red-500 appearance-none block p-2 pl-4 pr-12 bg-inherit mt-1 border border-gray-400 rounded-lg  '
+								name='select'
+								id='select'>
+								<option disabled selected hidden value='opcao1'>
+									Todos
+								</option>
+								<option value='opcao2'>B</option>
+								<option value='opcao3'>C</option>
+							</select>
+							<ChevronDownIcon className='w-4 absolute right-4 top-1/2 -translate-y-1/2' />
+						</div>
+					</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+					<button className='hidden lg:block border border-purple-700 px-6 py-2 rounded-full text-purple-700 font-semibold text-sm '>
+						<div className='flex items-center '>
+							<PlusIcon className='w-6' />
+							Adicionar obra
+						</div>
+					</button>
+				</div>
+
+				<div className=' gap-6 grid lg:grid-cols-2 grid-cols-1 place-items-center '>
+					<RoomCard complete />
+					<RoomCard />
+					<RoomCard />
+					<RoomCard />
+					<RoomCard complete />
+				</div>
+			</div>
+		</main>
+	);
 }
---
+++ src/components/card/RoomCard.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Room from '/public/room.jpg';
+import Image from 'next/image';
+import { ChevronRightIcon } from '@heroicons/react/24/solid';
+import { DocumentTextIcon } from '@heroicons/react/24/outline';
+
+interface RoomCardInterface {
+	className?: string;
+	complete?: boolean;
+}
+
+export default function RoomCard(props: RoomCardInterface) {
+	return (
+		<div className='bg-white  lg:px-0 px-3'>
+			<Image src={Room} alt='room' className='h-48 ' />
+			<div className='p-4'>
+				<div className='flex justify-between items-start'>
+					<div className=' font-medium flex flex-col items-start gap-2'>
+						<p className='font-bold'>Obras - Flores</p>
+						<p className='text-purple-900'>
+							Rua das Flores, 123, Vila Olimpia, Sao Paulo, SP
+						</p>
+						<p className='text-gray-500'>Responsavel Teresa Claire</p>
+						<p
+							className={`${
+								props.complete
+									? 'text-green-600   border-green-600'
+									: 'text-orange-400   border-orange-400'
+							} rounded-full px-2 font-bold border-2 text-sm`}>
+							{props.complete ? 'Concluida' : 'Em andamento'}
+						</p>
+					</div>
+					<ChevronRightIcon className='w-6 text-gray-400' />
+				</div>
+				<div className='mt-8 grid lg:grid-cols-3 lg:grid-rows-2 grid-cols-2 font-bold gap-2'>
+					<div className='py-3 gap-2 flex items-center flex-col  border text-center  shadow-md row-span-2'>
+						<div className='bg-purple-800/20 p-1 rounded-full'>
+							<DocumentTextIcon className='w-6 text-purple-800   rounded-full' />
+						</div>
+
+						<p className='text-sm text-gray-400'>RELATORIOS TOTAIS</p>
+						<p className='text-purple-800 text-2xl'>10</p>
+					</div>
+					<div className='py-3 gap-2 flex items-center flex-col  border text-center   '>
+						<p className='text-sm text-gray-400'>LEMBRETES</p>
+						<p className='text-purple-800 text-2xl'>5</p>
+					</div>
+					<div className='py-3 gap-2 flex items-center flex-col  border text-center   '>
+						<p className='text-sm text-gray-400'>OCORRÊNCIAS</p>
+						<p className='text-purple-800 text-2xl'>2</p>
+					</div>
+					<div className='py-3 gap-2 flex items-center flex-col  border text-center   '>
+						<p className='text-sm text-gray-400'>FOTOS</p>
+						<p className='text-purple-800 text-2xl'>2</p>
+					</div>
+					<div className='py-3 gap-2 flex items-center flex-col  border text-center   '>
+						<p className='text-sm text-gray-400'>VÍDEOS</p>
+						<p className='text-purple-800 text-2xl'>3</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
